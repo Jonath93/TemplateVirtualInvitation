4d8caaf7ee669ad3f77889311a425b26486b7b7f
Jonathan Benitez Barriga <jonathan_beni93@hotmail.com>
2025-02-05 22:53:13 -0700

    main template wedding

---
+++ src/scripts/main.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#txt_count_down');
+    countdownTimer(fiveMinutes, display);
+}
+function countdownTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
